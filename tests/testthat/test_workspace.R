f4b7a76cfa28f71f1fbb51adbe1948875c61070b
Heemanshu Suri <hesuri@microsoft.com>
2019-08-14 15:57:33 -0700

    Adding test to create and delete workspace Retriggering check

--- tests/testthat/test_workspace.R
+++ tests/testthat/test_workspace.R
@@ -2,14 +2,18 @@ context("Workspace")
 
 subscription_id <- Sys.getenv("TEST_SUBSCRIPTION_ID")
 resource_group <- Sys.getenv("TEST_RESOURCE_GROUP")
+location <- Sys.getenv("TEST_LOCATION")
 
-test_that("create, get, save and load workspace",
+test_that("create, get, save, load and delete workspace",
 {
-    workspace_name <- existing_ws$name
+    # create workspace
+    workspace_name <- "testws"
+    existing_ws <- create_workspace(workspace_name, subscription_id = subscription_id, resource_group = resource_group,
+                                    location = location)
 
     # retrieve workspace
     ws <- get_workspace(workspace_name, subscription_id = subscription_id, resource_group = resource_group)
-    expect_equal(ws$name, workspace_name)
+    expect_equal(ws$name, existing_ws$name)
 
     # write config
     write_workspace_config(existing_ws)
@@ -17,4 +21,7 @@ test_that("create, get, save and load workspace",
     # load from config
     loaded_ws <- load_workspace_from_config(".")
     expect_equal(loaded_ws$name, workspace_name)
+    
+    # delete workspace
+    delete_workspace(existing_ws)
 })
