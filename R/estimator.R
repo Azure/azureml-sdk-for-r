cf76060e9249115b9d3022ba9c247eb5df833c0a
heemanshusuri <51678909+heemanshusuri@users.noreply.github.com>
2019-08-17 09:04:27 -0700

    Adding launch script for estimators in case users provide extra packages to install (#12)
    * Adding launch script in case users provide extra packages to install
    
    * Adding tests for packages
    
    * Fixing script path
    
    * Typo using ggplot2
    
    * Resolving PR comments
    
    * Resolving PRcomments
    
    * Adding documentation
    
    * Fixing comment
    
    * renaming dummy train file

--- R/estimator.R
+++ R/estimator.R
@@ -11,27 +11,67 @@
 #' @param entry_script A string representing the relative path to the file used to start training.
 #' @param script_params A named list containing parameters to the entry_script.
 #' @param use_docker A bool value indicating if the environment to run the experiment should be docker-based.
+#' @param cran_packages character vector of cran packages to be installed.
+#' @param github_packages character vector of github packages to be installed.
+#' @param custom_url_packages character vector of packages to be installed from local, directory or custom url.
 #' @param custom_docker_image The name of the docker image from which the image to use for training will be built. If
 #' not set, a default CPU based image will be used as the base image.
 #' @param inputs list of data references as input
 #' @export
 create_estimator <- function(source_directory, compute_target = NULL, vm_size = NULL, vm_priority = NULL,
-                    entry_script = NULL, script_params = NULL, use_docker = TRUE,
-                    custom_docker_image = NULL, inputs = NULL)
+                             entry_script = NULL, script_params = NULL, use_docker = TRUE, cran_packages = NULL,
+                             github_packages = NULL, custom_url_packages = NULL,
+                             custom_docker_image = NULL, inputs = NULL)
+{ 
+  launch_script <- create_launch_script(source_directory, entry_script, cran_packages, github_packages, custom_url_packages)
+  estimator <- azureml$train$estimator$Estimator(source_directory, compute_target = compute_target, vm_size = vm_size,
+                                                 vm_priority = vm_priority, entry_script = launch_script, script_params = script_params, use_docker = use_docker,
+                                                 custom_docker_image = custom_docker_image, inputs = inputs)
+  
+  run_config <- estimator$run_config
+  run_config$framework <- "R"
+  run_config$environment$python$user_managed_dependencies <- TRUE
+  
+  if (is.null(custom_docker_image))
+  {
+    run_config$environment$docker$base_image <- "r-base:cpu"
+    run_config$environment$docker$base_image_registry$address <- "viennaprivate.azurecr.io"
+  }
+  
+  invisible(estimator)
+
+}
+
+#' Creates a R launch script which contains all the packages to be installed before running entry_script
+#' @param source_directory A local directory containing experiment configuration files.
+#' @param entry_script A string representing the relative path to the file used to start training.
+#' @param cran_packages character vector of cran packages to be installed.
+#' @param github_packages character vector of github packages to be installed.
+#' @param custom_url_packages character vector of packages to be installed from local, directory or custom url.
+create_launch_script <- function(source_directory, entry_script, cran_packages = NULL, github_packages = NULL, custom_url_packages = NULL)
 {
-    estimator <- azureml$train$estimator$Estimator(source_directory, compute_target = compute_target, vm_size = vm_size,
-        vm_priority = vm_priority, entry_script = entry_script, script_params = script_params, use_docker = use_docker,
-        custom_docker_image = custom_docker_image, inputs = inputs
-    )
-    run_config <- estimator$run_config
-    run_config$framework <- "R"
-    run_config$environment$python$user_managed_dependencies <- TRUE
-
-    if (is.null(custom_docker_image))
-    {
-        run_config$environment$docker$base_image <- "r-base:cpu"
-        run_config$environment$docker$base_image_registry$address <- "viennaprivate.azurecr.io"
-    }
-
-    invisible(estimator)
+  launch_file_name <- "launcher.R"
+  launch_file_conn <- file(file.path(source_directory, launch_file_name), open = "w")
+  
+  writeLines("# This is the auto-generated launcher file.\n# It installs the packages specified in the estimator.\n# Once all the packages are successfully installed, it will execute the entry script.\n", launch_file_conn)
+  
+  if (!is.null(cran_packages))
+  {
+    writeLines(sprintf("install.packages(\"%s\", repos = \"http://cran.us.r-project.org\")\n", cran_packages), launch_file_conn)
+  }
+  
+  if (!is.null(github_packages))
+  {
+    writeLines(sprintf("devtools::install_github(\"%s\")\n", github_packages), launch_file_conn)
+  }
+  
+  if (!is.null(custom_url_packages))
+  {
+    writeLines(sprintf("install.packages(\"%s\", repos = NULL)\n", custom_url_packages), launch_file_conn)
+  }
+  
+  writeLines(sprintf("source(\"%s\")", entry_script), launch_file_conn)
+  
+  close(launch_file_conn)
+  invisible(launch_file_name)
 }
---
+++ tests/testthat/train_dummy.R
@@ -0,0 +1,7 @@
+# check if ggplot2 and dplyr are already installed
+library("ggplot2")
+library("dplyr")
+
+library(azureml)
+
+log_metric_to_run("test_metric", 0, get_current_run())
