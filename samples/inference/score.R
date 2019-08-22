f28170f768845a986e42c48b73a87104a230ea27
Billy Hu <ninhu@microsoft.com>
2019-08-21 17:45:50 -0700

    code refactoring per Colin's suggestion

--- samples/inference/score.R
+++ samples/inference/score.R
@@ -1,42 +1,33 @@
-# CAUTION: If init() call failed, the inference launcher will keep retrying
-# This results in packages repeatly get installed again and again :( So
-# 1) We should check package existence before install
-# 2) Have proper error handling in python wrapper is important
-install.packages("caret", repos = "http://cran.us.r-project.org")
-install.packages("e1071", repos = "http://cran.us.r-project.org")
-install.packages("jsonlite", repos = "http://cran.us.r-project.org")
 library(jsonlite)
-library("azureml")
+library(azureml)
 
 model <- NULL
 
-init <- function()
+init <- function(model_path)
 {
-    # Note: below code is not working if source directory is specified in InferenceConfig.
-    # azureml$core$model$Model$get_model_path('model.rds')
-    # Fired Bug 494437: get_model_path('model.rds') throw execption
-    # For POC, use hard-coded path to workaround
-    model_path <- "/var/azureml-app/azureml-models/model.rds/1/model.rds"
+    # get_model_path('model.rds') doesn't work in R session for some reason
+    # Fired Bug 494437 for investigation
+    # As a workaround, pass in model_path through init()
     model <<- readRDS(model_path)
     message("model is loaded")
-}
 
-run <- function(data)
-{
-    plant <- as.data.frame(fromJSON(data))
-    prediction <- predict(model, plant)
-    result <- as.character(prediction)
+    function(data)
+    {
+        plant <- as.data.frame(fromJSON(data))
+        prediction <- predict(model, plant)
+        result <- as.character(prediction)
 
-    # object conversion b/w R and python is chanllenge with rpy2
-    # returned R data will be converted to a rpy2 vector object in python
-    # and this kind of object is not JSON serializible.
-    # https://rpy2.readthedocs.io/en/version_2.8.x/robjects_serialization.html
+        # object conversion b/w R and python is chanllenge with rpy2
+        # returned R data will be converted to a rpy2 vector object in python
+        # and this kind of object is not JSON serializible.
+        # https://rpy2.readthedocs.io/en/version_2.8.x/robjects_serialization.html
 
-    # The workaround is to convert to JSON in R first althought it's not the best option
-    # https://stackoverflow.com/questions/43010705/rpy2-convert-a-r-return-list-vector-to-a-json-object-in-python
-    toJSON(result)
+        # The workaround is to convert to JSON in R first althought it's not the best option
+        # https://stackoverflow.com/questions/43010705/rpy2-convert-a-r-return-list-vector-to-a-json-object-in-python
+        toJSON(result)
 
-    # plumber supports different response type like JSON, binary, HTML, etc
-    # so it is definitely the long term solution but it requires support from inference team first
-    # https://www.rplumber.io/docs/rendering-and-output.html#serializers
+        # plumber supports different response type like JSON, binary, HTML, etc
+        # so it is definitely the long term solution but it requires support from inference team first
+        # https://www.rplumber.io/docs/rendering-and-output.html#serializers
+    }
 }
--- samples/inference/score.py
+++ samples/inference/score.py
@@ -2,15 +2,31 @@
 import rpy2.robjects as robjects
 import os
 import json
+from azureml.core.model import Model
 
 def init():
+    global r_run
+
+    # install packages
+    # CAUTION: If init() call failed, the inference launcher will keep retrying
+    # This results in packages repeatly get installed again and again :( So
+    # 1) check package existence before install
+    # 2) have proper error handling in python wrapper is important
+    robjects.r('''
+        install.packages("caret", repos = "http://cran.us.r-project.org")
+        install.packages("e1071", repos = "http://cran.us.r-project.org")
+        install.packages("jsonlite", repos = "http://cran.us.r-project.org")
+        ''')
+
     score_r_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "score.R")
     # handle path for windows os
     score_r_path = score_r_path.replace('\\', '/')
     robjects.r('''source('{}')'''.format(score_r_path))
-    robjects.r['init']()
+
+    model_path = Model.get_model_path('model.rds')
+    r_run = robjects.r['init'](model_path)
 
 
 def run(input_data):
-    dataR = robjects.r['run'](input_data)[0]
+    dataR = r_run(input_data)[0]
     return json.loads(dataR)
