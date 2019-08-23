21978efe77ca5ecc681e94548ff45dd682b8b5af
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2019-08-22 21:12:57 -0700

    edit names

--- R/hyperdrive.R
+++ R/hyperdrive.R
@@ -23,37 +23,37 @@ create_hyperdrive_config <- function(estimator, param_sampling,
                                             max_concurrent_runs = max_concurrent_runs)
 }
 
-#' Get Bandit policy for HyperDrive runs
+#' Create Bandit policy for HyperDrive runs
 #' @param slack_factor ratio of the allowed distance from best-performing run
 #' @param slack_amount absolute allowed distance from the best-performing run
 #' @param evaluation_interval frequency for applying policy
 #' @param delay_evaluation how many intervals to delay the first evaluation
 #' @return Early Termination Policy object
 #' @export
-get_bandit_policy <- function(slack_factor = NULL, slack_amount = NULL,
+create_bandit_policy <- function(slack_factor = NULL, slack_amount = NULL,
                               evaluation_interval = 1, delay_evaluation = 0)
 {
   azureml$train$hyperdrive$BanditPolicy(slack_factor, slack_amount,
                                         evaluation_interval, delay_evaluation)
 }
 
-#' Get Median Stopping policy for HyperDrive runs
+#' Create Median Stopping policy for HyperDrive runs
 #' @param evaluation_interval frequency for applying policy
 #' @param delay_evaluation how many intervals to delay the first evaluation
 #' @return Early Termination Policy object
 #' @export
-get_median_stopping_policy <- function(evaluation_interval = 1, delay_evaluation = 0)
+create_median_stopping_policy <- function(evaluation_interval = 1, delay_evaluation = 0)
 {
   azureml$train$hyperdrive$MedianStoppingPolicy(evaluation_interval, delay_evaluation)
 }
 
-#' Get Truncation Selection policy for HyperDrive runs
+#' Create Truncation Selection policy for HyperDrive runs
 #' @param truncation_percentage percentage of lowest performing runs to terminate at each interval
 #' @param evaluation_interval frequency for applying policy
 #' @param delay_evaluation how many intervals to delay the first evaluation
 #' @return Early Termination Policy object
 #' @export
-get_truncation_selection_policy <- function(truncation_percentage,
+create_truncation_selection_policy <- function(truncation_percentage,
                                             evaluation_interval = 1, delay_evaluation = 0)
 {
   azureml$train$hyperdrive$TruncationSelectionPolicy(truncation_percentage,
