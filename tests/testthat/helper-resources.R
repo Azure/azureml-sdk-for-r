652a7682e333379e17d103cb42e9c3284b637db2
Heemanshu Suri <hesuri@microsoft.com>
2019-08-23 23:53:37 -0700

    Making resource group static

--- tests/testthat/helper-resources.R
+++ tests/testthat/helper-resources.R
@@ -2,7 +2,7 @@
 subscription_id <- Sys.getenv("TEST_SUBSCRIPTION_ID")
 resource_group <- Sys.getenv("TEST_RESOURCE_GROUP")
 location <- Sys.getenv("TEST_LOCATION")
-workspace_name <- Sys.getenv("TEST_WORKSPACE_NAME", unset = "r_test_workspace")
+workspace_name <- Sys.getenv("TEST_WORKSPACE_NAME", unset = "r_sdk_workspace")
 cluster_name <- Sys.getenv("TEST_CLUSTER_NAME", unset = "r-cpu-cluster")
 test_env <- paste0('test_', as.integer(Sys.time()))
 package_url <- Sys.getenv('PACKAGE_LOCATION')
@@ -16,13 +16,13 @@ if(is.na(Sys.getenv("AZUREML_PYTHON_INSTALLED", unset = NA)))
 }
 
 existing_ws <- create_workspace(workspace_name, subscription_id = subscription_id, resource_group = resource_group,
-                    location = location, exist_ok = TRUE)
+                                location = location, exist_ok = TRUE)
 
 existing_compute <- get_compute(workspace = existing_ws, cluster_name = cluster_name)
 if (is.null(existing_compute))
 {
   vm_size <- "STANDARD_D2_V2"
-  existing_compute <- create_aml_compute(workspace = existing_ws, cluster_name = cluster_name, vm_size = vm_size,
-                  min_nodes = 0, max_nodes = 1)
+  existing_compute <- create_aml_compute(workspace = existing_ws, cluster_name = cluster_name, 
+                                         vm_size = vm_size, min_nodes = 0, max_nodes = 1)
   wait_for_compute(existing_compute)
 }
