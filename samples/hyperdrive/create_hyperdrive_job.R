bbb7bc8c5fdf365cc2e9c14bfd989396bc6fc0f0
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2019-08-26 16:03:22 -0700

    update job script

--- samples/hyperdrive/create_hyperdrive_job.R
+++ samples/hyperdrive/create_hyperdrive_job.R
@@ -1,7 +1,7 @@
 # run setup.R prior to running this script
 library("azureml")
 
-ws <- load_workspace_from_config("~/azureml-sdk-for-r/samples/.azureml/config.json")
+ws <- load_workspace_from_config()
 
 # create aml compute
 cluster_name <- "rcluster"
@@ -15,8 +15,12 @@ if (is.null(compute_target))
 wait_for_compute(compute_target)
 
 # define estimator
-est <- create_estimator(source_directory = "./hyperdrive", entry_script = "hyperdrive_script.R",
-                        compute_target = compute_target, cran_packages = c("tensorflow"))
+script_params <- list(step = 0.01, num_octave = 3, octave_scale = 1.4, iterations = 20,
+                      max_loss = 10)
+
+est <- create_estimator(source_directory = ".", entry_script = "hyperdrive_script.R",
+                        compute_target = compute_target, script_params = script_params,
+                        cran_packages = c("keras"))
 
 experiment_name <- "hyperparameter-tuning-on-remote-amlcompute"
 exp <- experiment(ws, experiment_name)
@@ -28,10 +32,14 @@ metrics <- get_run_metrics(run)
 metrics
 
 # define sampling and policy for hyperparameter tuning
-sampling <- random_parameter_sampling(list(param1 = "", param2 = ""))
-policy <- create_bandit_policy()
-hyperdrive_config <- create_hyperdrive_config(est, sampling, "Accuracy",
-                                              "MAXIMIZE", 10, policy = policy)
+sampling <- random_parameter_sampling(list(step = normal(0.05, 0.01, 0.02),
+                                           num_octave = choice(2, 3, 5),
+                                           octave_scale = loguniform(0.4, 1.4, 2.4),
+                                           iterations = choice(10, 20, 30),
+                                           max_loss = choice(5, 10, 5)))
+policy <- bandit_policy(slack_factor = 0.15)
+hyperdrive_config <- create_hyperdrive_config(sampling, "Accuracy", "MAXIMIZE", 10,
+                                              policy = policy, estimator = est)
 
 # submit hyperdrive run
 hyperdrive_run <- submit_experiment(config = hyperdrive_config, exp)
