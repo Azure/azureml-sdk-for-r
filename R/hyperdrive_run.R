c5e1ce85e27e6b8bcf8c7557150aa2b72181cb7a
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2019-08-26 10:20:40 -0700

    add all hyperdrive_run functions

--- R/hyperdrive_run.R
+++ R/hyperdrive_run.R
@@ -1,31 +1,52 @@
 #' Return the best performing run amongst all completed runs
+#' @param hyperdrive_run HyperDriveRun object
 #' @param include_failed whether to include failed runs
 #' @param include_canceled whether to include canceled runs
 #' @return Run object
 #' @export
-get_best_run_by_primary_metric <- function(include_failed = TRUE, include_canceled = TRUE)
+get_best_run_by_primary_metric <- function(hyperdrive_run, include_failed = TRUE,
+                                           include_canceled = TRUE)
 {
   azureml$train$hyperdrive$get_best_run_by_primary_metric(include_failed, include_canceled)
 }
 
 #' Return the child runs sorted in descending order by best primary metric
+#' @param hyperdrive_run HyperDriveRun object
 #' @param top number of top children to be returned, deafult value of 0 will return all
 #' @param reverse reverse the sorting order
 #' @param discard_no_metric whether to include children without the primary metric
-#' @return **TODO: (find exact type of list)** Run object
+#' @return named list of child runs
 #' @export
-get_children_sorted_by_primary_metric <- function(top = 0, reverse = FALSE, discard_no_metric = FALSE)
+get_children_sorted_by_primary_metric <- function(hyperdrive_run, top = 0,
+                                                  reverse = FALSE, discard_no_metric = FALSE)
 {
-  azureml$train$hyperdrive$get_children_sorted_by_primary_metric(top, reverse, discard_no_metric)
+  azureml$train$hyperdrive$get_children_sorted_by_primary_metric(top, reverse,
+                                                                 discard_no_metric)
 }
 
-#' Return the child runs sorted in descending order by best primary metric
-#' @param top number of top children to be returned, deafult value of 0 will return all
-#' @param reverse reverse the sorting order
-#' @param discard_no_metric whether to include children without the primary metric
-#' @return **TODO: (find exact type of list)** Run object
+#' Cancel HyperDrive run
+#' @param hyperdrive_run HyperDriveRun object to be cancelled
+#' @return TRUE if cancellation was successful, else FALSE
 #' @export
-get_children_sorted_by_primary_metric <- function(top = 0, reverse = FALSE, discard_no_metric = FALSE)
+cancel_hyperdrive_run <- function(hyperdrive_run)
 {
-  azureml$train$hyperdrive$get_children_sorted_by_primary_metric(top, reverse, discard_no_metric)
+  azureml$train$hyperdrive$cancel(hyperdrive_run)
+}
+
+#' Return hyperparameters for all child runs
+#' @param hyperdrive_run HyperDriveRun object
+#' @return named list of hyperparameters grouped by run_id
+#' @export
+get_children_hyperparameters <- function(hyperdrive_run)
+{
+  azureml$train$hyperdrive$get_hyperparameters(hyperdrive_run)
+}
+
+#' Return metrics from all child runs
+#' @param hyperdrive_run HyperDriveRun object
+#' @return name list of metrics grouped by run_id
+#' @export
+get_children_metrics <- function(hyperdrive_run)
+{
+  azureml$train$hyperdrive$get_metrics(hyperdrive_run)
 }
