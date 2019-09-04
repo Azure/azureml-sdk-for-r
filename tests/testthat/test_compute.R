3e5f932eb431da841d37f232878d416c79853dfa
Heemanshu Suri <hesuri@microsoft.com>
2019-09-04 11:52:58 -0700

    Using build number for test resource names

--- tests/testthat/test_compute.R
+++ tests/testthat/test_compute.R
@@ -5,7 +5,7 @@ test_that("create amlcompute",
     ws <- existing_ws
 
     vm_size <- "STANDARD_D2_V2"
-    cluster_name <- paste("testcls", build_id, sep="")
+    cluster_name <- paste("cls", build_num, sep="")
     compute_target <- create_aml_compute(workspace = ws, cluster_name = cluster_name, vm_size = vm_size, max_nodes = 1)
     wait_for_compute(compute_target)
     expect_equal(compute_target$name, cluster_name)
