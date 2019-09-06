65c39f304aa599f19cd670c41fd564ec366ea840
Heemanshu Suri <hesuri@microsoft.com>
2019-09-06 14:07:48 -0700

    Adding tests for model

--- tests/testthat/test_model.R
+++ tests/testthat/test_model.R
@@ -9,21 +9,24 @@ test_that("get, register, download, serialize, deserialize and delete model",
     dir.create(tmp_dir_name)
     file.create(file.path(tmp_dir_name, model_name))
     
+    # register the model
     model <- register_model(ws, tmp_dir_name, model_name)
     
+    # get model
     ws_model <- get_model(ws, model_name)
+
     expect_equal(model_name, ws_model$name)
-    
+
+    # download model    
     download_dir <- "downloaded"
     dir.create(download_dir)
     path <- download_model(model, download_dir)
     expect_equal(file.exists(file.path(download_dir, tmp_dir_name, model_name)), TRUE)
     
-    serialized_dict <- serialize_model(model)
-    deserialized_model <- deserialize_to_model()
+    # serialize sndand deserialize model
+    model_payload <- serialize_model(model)
+    deserialized_model <- deserialize_to_model(ws, model_payload)
 
-
-
-    # tear down compute
-    delete_aml_compute(compute_target)
+    # delete the model
+    delete_model(model)
 })
