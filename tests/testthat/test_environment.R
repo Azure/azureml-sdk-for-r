28efa5eb69facf8cf53119644afcf1a3b96a9b32
Heemanshu Suri <hesuri@microsoft.com>
2019-09-12 14:09:24 -0700

    Fixing env test

--- tests/testthat/test_environment.R
+++ tests/testthat/test_environment.R
@@ -10,8 +10,9 @@ test_that("create, register, and get environment",
     env <- environment(env_name, version = "1")
     expect_equal(env$name, env_name)
     expect_equal(env$version, "1")
-    expect_equal(env$docker$base_image, NULL)
-    expect_equal(env$docker$base_dockerfile, "FROM viennaprivate.azurecr.io/r-base:cpu\n")
+    
+    expect_equal(env$docker$base_dockerfile, NULL)
+    expect_equal(env$docker$base_image, "r-base:cpu")
 
     # Register environment
     register_environment(env, ws)
