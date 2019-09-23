dd2ffdf017ed3fc448dbb1852bc9901c89b6318b
Billy Hu <ninhu@microsoft.com>
2019-09-23 16:07:07 -0700

    add missing run apis

--- tests/testthat/test_run.R
+++ tests/testthat/test_run.R
@@ -29,10 +29,25 @@ test_that("create, submit experiment, run in default amlcompute, get run metrics
 
     run <- submit_experiment(est, exp)
     wait_for_run_completion(run, show_output = TRUE)
+    
     metrics <- get_run_metrics(run)
 
-    expected_metrics <- list("test_metric" = 0)
-    expect_equal(length(setdiff(metrics, expected_metrics)), 0)
+    expected_metrics <- list("test_metric" = 0.5)
+    expect_equal(metrics$test_metric, 0.5)
+    
+    # Uncomment once base image upgrades the sdk
+    #expected_list <- c(1, 2, 3)
+    #expect_equal(length(setdiff(metrics$test_list, expected_list)), 0)
+    
+    #expected_row <- list(x = 1, y = 2)
+    #expect_equal(length(setdiff(metrics$test_row, expected_row)), 0)
+    
+    #expect(startsWith(metrics$test_predictions, "aml://artifactId") &&
+    #       endsWith(metrics$test_predictions, "test_predictions"), 
+    #       "invalid predictions uri returned")
+    
+    files <- get_run_file_names(run)
+    expect(length(files) > 0, "No file is generated from the run")
 
     # tear down resources
     unlink(tmp_dir_name, recursive = TRUE)
--- tests/testthat/train_empty.R
+++ tests/testthat/train_empty.R
@@ -1,3 +1,18 @@
 library(azureml)
 
-log_metric_to_run("test_metric", 0, get_current_run())
+log_metric_to_run("test_metric", 0.5)
+
+# Uncomment once base image upgrades the sdk
+#log_list_to_run("test_list", c(1, 2, 3))
+#log_row_to_run("test_row", x = 1, y = 2)
+#predict_json <- '{
+#                    "schema_type": "predictions",
+#                    "schema_version": "v1",
+#                    "data": {
+#                        "bin_averages": [0.25, 0.75],
+#                        "bin_errors": [0.013, 0.042],
+#                        "bin_counts": [56, 34],
+#                        "bin_edges": [0.0, 0.5, 1.0]
+#                    }
+#                }'
+#log_predictions_to_run("test_predictions", predict_json)
