aa4e38f2adc0487e833a0cd8f0fe982a609901dc
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2019-09-24 13:15:48 -0700

    update string 2

--- tests/testthat/test_dockerfile.R
+++ tests/testthat/test_dockerfile.R
@@ -6,7 +6,8 @@ test_that("create dockerfile", {
 
     dockerfile <- generate_docker_file(custom_docker_image = "ubuntu-18.04",
                                        cran_packages = c("ggplot2"))
-    expect_equal(dockerfile, paste0("FROM ubuntu-18.04\nRUN R -e",
-                                    "install.packages(\"ggplot2\", repos = ",
-                                    "\"http://cran.us.r-project.org\")\n"))
+    expected_dockerfile <- paste0("FROM ubuntu-18.04\nRUN R -e ",
+                                  "install.packages(\"ggplot2\",\n repos = ",
+                                  "\"http://cran.us.r-project.org\")\n")
+    expect_equal(dockerfile, expected_dockerfile)
 })
