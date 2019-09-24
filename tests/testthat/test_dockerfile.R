c2c8a97b7fadab6af516b041c38c217fd5cfb213
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2019-09-24 14:17:04 -0700

    fix spacing/paste function

--- tests/testthat/test_dockerfile.R
+++ tests/testthat/test_dockerfile.R
@@ -6,8 +6,12 @@ test_that("create dockerfile", {
 
     dockerfile <- generate_docker_file(custom_docker_image = "ubuntu-18.04",
                                        cran_packages = c("ggplot2"))
-    expected_dockerfile <- paste0("FROM ubuntu-18.04\nRUN R -e ",
-                                  "install.packages(\"ggplot2\",\n repos = ",
-                                  "\"http://cran.us.r-project.org\")\n")
+
+    expected_base_dockerfile <- "FROM ubuntu-18.04\n"
+    install_command <- sprintf("RUN R -e install.packages(\"%s\",
+                               repos = \"http://cran.us.r-project.org\")\n",
+                               "ggplot2")
+
+    expected_dockerfile <- paste0(expected_base_dockerfile, install_command)
     expect_equal(dockerfile, expected_dockerfile)
 })
