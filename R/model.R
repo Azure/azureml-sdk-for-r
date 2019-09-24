70462282612a0c0fc693cc8458cbbf882d8cf308
Heemanshu Suri <hesuri@microsoft.com>
2019-09-24 12:23:40 -0700

    Adding method to generate score.py file

--- R/model.R
+++ R/model.R
@@ -3,59 +3,86 @@
 
 #' Retrieve the Model object from the cloud.
 #' @param workspace The workspace object containing the Model to retrieve
-#' @param name Will retrieve the latest model with the corresponding name, if it exists
+#' @param name Will retrieve the latest model with the corresponding name, if 
+#' it exists
 #' @param id Will retrieve the model with the corresponding ID, if it exists
-#' @param tags Optional, will filter based on the provided list, searching by either 'key' or '[key, value]'.
-#' @param properties Optional, will filter based on the provided list, searching by either 'key' or '[key, value]'.
-#' @param version When provided along with name, will get the specific version of the specified named model, if it exists
+#' @param tags Optional, will filter based on the provided list, searching by 
+#' either 'key' or '[key, value]'.
+#' @param properties Optional, will filter based on the provided list, 
+#' searching by either 'key' or '[key, value]'.
+#' @param version When provided along with name, will get the specific version 
+#' of the specified named model, if it exists
 #' @param run_id Optional, will filter based on the provided ID.
+#' @return A model object, if one is found in the provided workspace
 #' @export
-get_model <- function(workspace, name = NULL, id = NULL, tags = NULL, 
-                      properties = NULL, version = NULL, run_id = NULL)
-{
-  model <- azureml$core$Model(workspace, name, id, tags, properties, 
-                              version, run_id)
+get_model <- function(workspace, 
+                      name = NULL, 
+                      id = NULL, 
+                      tags = NULL, 
+                      properties = NULL, 
+                      version = NULL, 
+                      run_id = NULL) {
+  model <- azureml$core$Model(workspace, 
+                              name, 
+                              id, 
+                              tags, 
+                              properties, 
+                              version, 
+                              run_id)
   invisible(model)
 }
 
 #' Register a model with the provided workspace.
 #' @param workspace The workspace to register the model under
-#' @param model_path String which points to the path on the local file system where the model assets are
-#' located. This can be a direct pointer to a single file or folder. If pointing to a folder, the
-#' child_paths parameter can be used to specify individual files to bundle together as the Model object,
+#' @param model_path String which points to the path on the local file system 
+#' where the model assets are located. This can be a direct pointer to a single 
+#' file or folder. If pointing to a folder, the child_paths parameter can be 
+#' used to specify individual files to bundle together as the Model object,
 #' as opposed to using the entire contents of the folder.
 #' @param model_name The name to register the model with
 #' @param tags Dictionary of key value tags to give the model
-#' @param properties Dictionary of key value properties to give the model. These properties cannot
-#' be changed after model creation, however new key value pairs can be added
+#' @param properties Dictionary of key value properties to give the model. 
+#' These properties cannot be changed after model creation, however new key 
+#' value pairs can be added
 #' @param description A text description of the model
-#' @param child_paths If provided in conjunction with a model_path to a folder, only the specified files will be
-#' bundled into the Model object.
+#' @param child_paths If provided in conjunction with a model_path to a folder, 
+#' only the specified files will be bundled into the Model object.
+#' @return The registered Model object
 #' @export
-register_model <- function(workspace, model_path, model_name, tags = NULL,
-                           properties = NULL, description = NULL, child_paths = NULL)
-{
-  model <- azureml$core$Model$register(workspace, model_path, model_name, tags = tags,
-                                       properties = properties, description = description,
+register_model <- function(workspace, 
+                           model_path, 
+                           model_name, 
+                           tags = NULL,
+                           properties = NULL, 
+                           description = NULL, 
+                           child_paths = NULL) {
+  model <- azureml$core$Model$register(workspace, 
+                                       model_path, 
+                                       model_name, 
+                                       tags = tags,
+                                       properties = properties, 
+                                       description = description,
                                        child_paths = child_paths)
   invisible(model)
 }
 
 #' Download model to target_dir of local file system.
 #' @param model The model to download
-#' @param target_dir Path to directory for where to download the model. Defaults to "."
-#' @param exist_ok Boolean to replace downloaded dir/files if exists. Defaults to False
+#' @param target_dir Path to directory for where to download the model. 
+#' Defaults to "."
+#' @param exist_ok Boolean to replace downloaded dir/files if exists. 
+#' Defaults to FALSE
+#' @return string path to file or folder of model
 #' @export
-download_model <- function(model, target_dir = '.', exist_ok = FALSE)
-{
+download_model <- function(model, target_dir = '.', exist_ok = FALSE) {
   model_path <- model$download(target_dir, exist_ok)
   invisible(model_path)
 }
 
 #' Convert this Model into a json serialized dictionary
 #' @param model The model to download
-serialize_model <- function(model)
-{
+#' @return The json representation of this Model
+serialize_model <- function(model) {
   result <- model$serialize()
   invisible(result)
 }
@@ -63,8 +90,8 @@ serialize_model <- function(model)
 #' Convert a json object into a Model object.
 #' @param workspace The workspace object the model is registered under
 #' @param model_payload A json object to convert to a Model object
-deserialize_to_model <- function(workspace, model_payload)
-{
+#' @return The Model representation of the provided json object
+deserialize_to_model <- function(workspace, model_payload) {
   model <- azureml$core$Model$deserialize(workspace, model_payload)
   invisible(model)
 }
@@ -72,43 +99,60 @@ deserialize_to_model <- function(workspace, model_payload)
 #' Delete this model from its associated workspace.
 #' @param model The model to download
 #' @export
-delete_model <- function(model)
-{
+delete_model <- function(model) {
   model$delete()
 }
 
 #' Deploy a Webservice from zero or more model objects.
 #' @param workspace A Workspace object to associate the Webservice with
-#' @param name The name to give the deployed service. Must be unique to the workspace, only consist of lowercase
-#' letters, numbers, or dashes, start with a letter, and be between 3 and 32 characters long.
+#' @param name The name to give the deployed service. Must be unique to the 
+#' workspace, only consist of lowercase letters, numbers, or dashes, start with 
+#' a letter, and be between 3 and 32 characters long.
 #' @param models A list of model objects. Can be an empty list.
-#' @param inference_config An InferenceConfig object used to determine required model properties.
-#' @param deployment_config A WebserviceDeploymentConfiguration used to configure the webservice. If one is not
-#' provided, an empty configuration object will be used based on the desired target.
-#' @param deployment_target A azureml.core.ComputeTarget to deploy the Webservice to.
-#' As Azure Container Instances has no associated azureml.core.ComputeTarget, leave
-#' this parameter as None to deploy to Azure Container Instances.
+#' @param inference_config An InferenceConfig object used to determine required 
+#' model properties.
+#' @param deployment_config A WebserviceDeploymentConfiguration used to 
+#' configure the webservice. If one is not provided, an empty configuration 
+#' object will be used based on the desired target.
+#' @param deployment_target A azureml.core.ComputeTarget to deploy the 
+#' Webservice to. As Azure Container Instances has no associated 
+#' azureml.core.ComputeTarget, leave this parameter as None to deploy to Azure 
+#' Container Instances.
+#' @return A Webservice object corresponding to the deployed webservice
 #' @export
-deploy_model <- function(workspace, name, models, inference_config,
-                         deployment_config = NULL, deployment_target = NULL)
-{
-  webservice <- azureml$core$Model$deploy(workspace, name, models, inference_config,
-                                          deployment_config, deployment_target)
+deploy_model <- function(workspace, 
+                         name, 
+                         models, 
+                         inference_config,
+                         deployment_config = NULL, 
+                         deployment_target = NULL) {
+  webservice <- azureml$core$Model$deploy(workspace, 
+                                          name, 
+                                          models, 
+                                          inference_config,
+                                          deployment_config, 
+                                          deployment_target)
   invisible(webservice)
 }
 
-#' Create a model package in the form of a Docker image or Dockerfile build context
+#' Create a model package in the form of a Docker image or Dockerfile build 
+#' context
 #' @param workspace The workspace in which to create the package.
-#' @param models A list of Model objects to include in the package. Can be an empty list.
-#' @param inference_config An InferenceConfig object to configure the operation of the models.
-#' This must include an Environment object.
-#' @param generate_dockerfile Whether to create a Dockerfile that can be run locally
-#' instead of building an image.
+#' @param models A list of Model objects to include in the package. Can be an 
+#' empty list.
+#' @param inference_config An InferenceConfig object to configure the 
+#' operation of the models. This must include an Environment object.
+#' @param generate_dockerfile Whether to create a Dockerfile that can be run 
+#' locally instead of building an image.
+#' @return A ModelPackage object.
 #' @export
-package_model <- function(workspace, models, inference_config,
-                          generate_dockerfile = FALSE)
-{
-  model_package <- azureml$core$Model$package(workspace, models, inference_config,
+package_model <- function(workspace, 
+                          models, 
+                          inference_config,
+                          generate_dockerfile = FALSE) {
+  model_package <- azureml$core$Model$package(workspace, 
+                                              models, 
+                                              inference_config,
                                               generate_dockerfile)
   invisible(model_package)
 }
@@ -118,8 +162,7 @@ package_model <- function(workspace, models, inference_config,
 #' @param package Package created with model(s) and dependencies.
 #' @return ContainerRegistry object
 #' @export
-get_model_package_container_registry <- function(package)
-{
+get_model_package_container_registry <- function(package) {
   package$get_container_registry()
 }
 
@@ -129,8 +172,9 @@ get_model_package_container_registry <- function(package)
 #' @param offset Byte offset from which to start reading the logs.
 #' @return Package creation logs.
 #' @export
-get_model_package_creation_logs <- function(package, decode = TRUE, offset = 0)
-{
+get_model_package_creation_logs <- function(package, 
+                                            decode = TRUE, 
+                                            offset = 0) {
   package$get_logs(decode, offset)
 }
 
@@ -138,8 +182,7 @@ get_model_package_creation_logs <- function(package, decode = TRUE, offset = 0)
 #' This can only be used with a Docker image package.
 #' @param package Package created with model(s) and dependencies.
 #' @export
-pull_model_package_image <- function(package)
-{
+pull_model_package_image <- function(package) {
   package$pull()
 }
 
@@ -149,8 +192,7 @@ pull_model_package_image <- function(package)
 #' @param output_directory Local directory that will be created to contain 
 #' the contents of the package.
 #' @export
-save_model_package_files <- function(package, output_directory)
-{
+save_model_package_files <- function(package, output_directory) {
   package$save(output_directory)
 }
 
@@ -158,27 +200,66 @@ save_model_package_files <- function(package, output_directory)
 #' @param package Package created with model(s) and dependencies.
 #' @param show_output Boolean option to print more verbose output. Defaults to FALSE.
 #' @export
-wait_for_model_package_creation <- function(package, show_output = FALSE)
-{
+wait_for_model_package_creation <- function(package, show_output = FALSE) {
   package$wait_for_creation(show_output)
 }
 
 #' Creates the Model deployment config specific to model deployments.
-#' @param entry_script Path to local file that contains the code to run for the image.
-#' @param source_directory paths to folders that contains all files to create the image.
+#' @param entry_script Path to local file that contains the code to run for the 
+#' image.
+#' @param source_directory paths to folders that contains all files to create 
+#' the image.
 #' @param description A description to give this image.
-#' @param environment An environment object to use for the deployment. Doesn't have to be registered. A user
-#' should provide either this, or the other parameters, not both. The individual parameters will NOT serve
-#' as an override for the environment object. Exceptions include `entry_script`, `source_directory` and
-#' `description`.
+#' @param environment An environment object to use for the deployment. 
+#' Doesn't have to be registered. A user should provide either this, or the 
+#' other parameters, not both. The individual parameters will NOT serve
+#' as an override for the environment object. Exceptions include 
+#' `entry_script`, `source_directory` and `description`.
 #' @return An InferenceConfig object
 #' @export
-inference_config <- function(entry_script, source_directory = NULL, 
-                             description = NULL, environment = NULL)
-{
-  inference_config <- azureml$core$model$InferenceConfig(entry_script = entry_script,
-                                                         source_directory = source_directory,
-                                                         description = description,
-                                                         environment = environment)
+inference_config <- function(entry_script, 
+                             source_directory = NULL, 
+                             description = NULL, 
+                             environment = NULL) {
+  generate_score_py(entry_script, source_directory)
+  inference_config <- azureml$core$model$InferenceConfig(
+    entry_script = "score.py",
+    source_directory = source_directory,
+    runtime = "python",
+    description = description,
+    environment = environment)
+
   invisible(inference_config)
+}
+
+#' Generate score.py file for the corresponding score.R file
+#' @param entry_script Path to local file that contains the code to run for 
+#' the image.
+#' @param source_directory paths to folders that contains all files to 
+#' create the image.
+generate_score_py <- function(entry_script, source_directory) {
+  score_py_template <- sprintf("# This is auto-generated python wrapper.
+import rpy2.robjects as robjects
+import os
+import json
+from azureml.core.model import Model
+
+def init():
+    global r_run
+
+    score_r_path = \"%s\"
+    # handle path for windows os
+    score_r_path = score_r_path.replace('\\\\', '/')
+    robjects.r('''source('{}')'''.format(score_r_path))
+
+def run(input_data):
+    dataR = r_run(input_data)[0]
+    return json.loads(dataR)", 
+                               entry_script_path)
+
+  score_py_file_path <- file.path(source_directory, "score.py")
+  py_file <- file(score_py_file_path, open = "w")
+  writeLines(score_py_template, py_file)
+  close(py_file)
+  invisible(NULL)
 }
