25961f08d3c4e6e1d77b723621fd45d8c33a1449
Billy Hu <ninhu@microsoft.com>
2019-09-24 15:15:19 -0700

    revert unwanted changes

--- samples/setup.R
+++ samples/setup.R
@@ -5,10 +5,10 @@ devtools::install_github('https://github.com/Azure/azureml-sdk-for-r')
 library(azureml)
 install_azureml()
 
-subscription_id <- Sys.getenv("SUBSCRIPTION_ID", unset = "13e50845-67bc-4ac5-94db-48d493a6d9e8")
-resource_group <- Sys.getenv("RESOURCE_GROUP", unset = "rg-ninhu-westeurope")
-workspace_name <- Sys.getenv("WORKSPACE_NAME", unset = "myworkspace")
-location <- Sys.getenv("WORKSPACE_REGION", unset = "westeurope")
+subscription_id <- Sys.getenv("SUBSCRIPTION_ID", unset = "<my-subscription-id>")
+resource_group <- Sys.getenv("RESOURCE_GROUP", unset = "<my-resource-group>")
+workspace_name <- Sys.getenv("WORKSPACE_NAME", unset = "<my-workspace-name>")
+location <- Sys.getenv("WORKSPACE_REGION", unset = "<my-region>")
 
 ws <- create_workspace(name = workspace_name, subscription_id = subscription_id, resource_group = resource_group,
                        location = location, create_resource_group = TRUE, exist_ok = TRUE)
