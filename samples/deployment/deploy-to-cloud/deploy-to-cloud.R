201cb792e1f489cf2e541fffffa93529d7ff6a78
Heemanshu Suri <hesuri@microsoft.com>
2019-09-28 13:46:09 -0700

    Adding spaces around =

--- samples/deployment/deploy-to-cloud/deploy-to-cloud.R
+++ samples/deployment/deploy-to-cloud/deploy-to-cloud.R
@@ -12,11 +12,12 @@ r_env <- environment(name = "r_env")
 # create inference config
 inference_config <- inference_config(
   entry_script = "score.R",
-  source_directory=".",
+  source_directory = ".",
   environment = r_env)
 
 # create ACI deployment config
-deployment_config <- aci_webservice_deployment_config(cpu_cores=1, memory_gb=1)
+deployment_config <- aci_webservice_deployment_config(cpu_cores = 1,
+                                                      memory_gb = 1)
 
 # deploy the webservice
 service <- deploy_model(ws, 
@@ -31,19 +32,19 @@ wait_for_deployment(service, show_output = TRUE)
 
 # Inferencing
 # versicolor
-plant <- data.frame(Sepal.Length=6.4, 
-                    Sepal.Width=2.8, 
-                    Petal.Length=4.6, 
-                    Petal.Width=1.8)
+plant <- data.frame(Sepal.Length = 6.4,
+                    Sepal.Width = 2.8,
+                    Petal.Length = 4.6,
+                    Petal.Width = 1.8)
 # setosa
-plant <- data.frame(Sepal.Length=5.1, 
-                    Sepal.Width=3.5, 
-                    Petal.Length=1.4, 
-                    Petal.Width=0.2)
+plant <- data.frame(Sepal.Length = 5.1,
+                    Sepal.Width = 3.5,
+                    Petal.Length = 1.4,
+                    Petal.Width = 0.2)
 # virginica
-plant <- data.frame(Sepal.Length=6.7, 
-                    Sepal.Width=3.3, 
-                    Petal.Length=5.2, 
-                    Petal.Width=2.3)
+plant <- data.frame(Sepal.Length = 6.7,
+                    Sepal.Width = 3.3,
+                    Petal.Length = 5.2,
+                    Petal.Width = 2.3)
 
 predicted_val <- invoke_webservice(service, toJSON(plant))
