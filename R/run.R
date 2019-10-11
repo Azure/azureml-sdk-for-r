6864cf5dd650723d6789d42e1beedf3e45a3510b
Billy Hu <ninhu@microsoft.com>
2019-10-11 09:39:28 -0700

    Adding wrappers for upload_files and upload_folder for Run

--- R/run.R
+++ R/run.R
@@ -319,3 +319,39 @@ view_run_details <- function(run) {
                 caption = paste(unlist(details$warnings), collapse = "\r\n"),
                 options = list(dom = "t", scrollY = TRUE))
 }
+
+#' Upload files to the run record.
+#' @param run Run object.
+#' @param names The names of the files to upload. If set, paths must also be 
+#' set.
+#' @param paths The relative local paths to the files to upload. If set, names 
+#' is required.
+#' @param timeout_seconds The timeout for uploading files.
+#' @export
+upload_files_to_run <- function(run, names, paths, timeout_seconds = NULL) {
+  if (is.null(run)) {
+    run <- get_current_run()
+  }
+  
+  run$upload_files(
+    names = names, 
+    paths = paths, 
+    timeout_seconds = timeout_seconds)
+  
+  invisible(NULL)
+}
+
+#' Upload the specified folder to the given prefix name.
+#' @param run Run object.
+#' @param name The name of the folder of files to upload.
+#' @param path The relative local path to the folder to upload.
+#' @export
+upload_folder_to_run <- function(run, name, path) {
+  if (is.null(run)) {
+    run <- get_current_run()
+  }
+  
+  run&upload_folder(name, path)
+  
+  invisible(NULL) 
+}
--- tests/testthat/test_estimator.R
+++ tests/testthat/test_estimator.R
@@ -22,7 +22,7 @@ test_that("create, submit experiment, run in default amlcompute,
                    compute_target = existing_compute$name, 
                    entry_script = script_name, 
                    script_params = list("data_folder" = ds$as_mount()),
-                   cran_packages = c("ggplot2", "dplyr"))
+                   cran_packages = c("dplyr", "ggplot2"))
   
   run <- submit_experiment(exp, est)
   wait_for_run_completion(run, show_output = TRUE)
@@ -30,22 +30,22 @@ test_that("create, submit experiment, run in default amlcompute,
   run <- get_run(exp, run$id)
   metrics <- get_run_metrics(run)
   
-  expected_metrics <- list("test_metric" = 0.5)
-  expect_equal(length(setdiff(metrics, expected_metrics)), 0)
+  expect_equal(metrics$test_metric, 0.5)
   
-  # Uncomment once base image upgrades the sdk
-  # expected_list <- c(1, 2, 3)
-  # expect_equal(length(setdiff(metrics$test_list, expected_list)), 0)
+  expected_list <- c(1, 2, 3)
+  expect_equal(length(setdiff(metrics$test_list, expected_list)), 0)
 
-  # expected_row <- list(x = 1, y = 2)
-  # expect_equal(length(setdiff(metrics$test_row, expected_row)), 0)
+  expected_row <- list(x = 1, y = 2)
+  expect_equal(length(setdiff(metrics$test_row, expected_row)), 0)
 
-  # expect(startsWith(metrics$test_predictions, "aml://artifactId") &&
-  #       endsWith(metrics$test_predictions, "test_predictions"),
-  #       "invalid predictions uri returned")
+  expect(startsWith(metrics$test_predictions, "aml://artifactId") &&
+         endsWith(metrics$test_predictions, "test_predictions"),
+         "invalid predictions uri returned")
+  
+  upload_files_to_run(run, list("dummy_data"), list("dummy_data.txt"))
 
   files <- get_run_file_names(run)
-  expect(length(files) > 0, "No file is generated from the run")
+  expect_true("dummy_data" %in% files)
   
   # tear down resources
   unlink(tmp_dir_name, recursive = TRUE)
@@ -60,7 +60,7 @@ test_that("submit experiment through a custom environment", {
   dir.create(tmp_dir_name)
   file.copy(script_name, tmp_dir_name)
   
-  env <- r_environment("myenv", cran_packages = c("ggplot2", "dplyr"))
+  env <- r_environment("myenv", cran_packages = c("dplyr", "ggplot2"))
 
   est <- estimator(tmp_dir_name,
                    compute_target = existing_compute$name, 
