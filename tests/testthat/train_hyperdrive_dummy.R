4b4d0341d37104290a48f5a1e69ef23561b9d0a5
Billy Hu <ninhu@microsoft.com>
2019-10-18 21:11:44 -0700

    fix the broken hyperdriver test

--- tests/testthat/train_hyperdrive_dummy.R
+++ tests/testthat/train_hyperdrive_dummy.R
@@ -2,9 +2,9 @@ library(azuremlsdk)
 
 args <- commandArgs(trailingOnly = TRUE)
 number_1 <- args[2]
-log_metric_to_run("First Number", number_1, run)
+log_metric_to_run("First Number", number_1)
 number_2 <- args[4]
-log_metric_to_run("Second Number", number_2, run)
+log_metric_to_run("Second Number", number_2)
 
 sum <- as.numeric(number_1) + as.numeric(number_2)
 log_metric_to_run("Sum", sum)
