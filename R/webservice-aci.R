34de8b1dac1f615c1398e963c0c9f71091ef76ff
Billy Hu <ninhu@microsoft.com>
2019-10-22 16:34:28 -0700

    fix code style issue

--- R/webservice-aci.R
+++ R/webservice-aci.R
@@ -6,7 +6,7 @@
 #' Deploy a web service to Azure Container Instances for testing or
 #' debugging. Use ACI for low-scale CPU-based workloads that
 #' require less than 48 GB of RAM.
-#' 
+#'
 #' Deploy to ACI if one of the following conditions is true:
 #' * You need to quickly deploy and validate your model. You do not need
 #' to create ACI containers ahead of time. They are created as part of
@@ -81,7 +81,7 @@ aci_webservice_deployment_config <- function(cpu_cores = NULL,
 #' Update an ACI web service with the provided properties. You can update the
 #' web service to use a new model, a new entry script, or new dependencies
 #' that can be specified in an inference configuration.
-#' 
+#'
 #' Values left as `NULL` will remain unchanged in the web service.
 #' @param webservice The `AciWebservice` object.
 #' @param tags A named list of key-value tags for the web service,
