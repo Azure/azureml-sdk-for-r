dfa636a98fae61f2d26d154f1c53fa1a103caa3a
Heemanshu Suri <hesuri@microsoft.com>
2019-10-23 17:28:04 -0700

    Adding rpy2 package in inference config

--- R/model.R
+++ R/model.R
@@ -487,6 +487,9 @@ inference_config <- function(entry_script,
     description = description,
     environment = environment)
 
+  inference_config$environment$python$conda_dependencies$add_pip_package(
+    "rpy2"
+  )
   inference_config$environment$docker$base_image <- saved_image
   invisible(inference_config)
 }
