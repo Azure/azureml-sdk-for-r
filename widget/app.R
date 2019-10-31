29ceec417c665bfc82a3147405cb40563b66693b
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2019-10-31 13:38:32 -0700

    restructure app

--- widget/app.R
+++ widget/app.R
@@ -6,63 +6,62 @@ suppressMessages(library(shinycssloaders))
 server <- function(input, output, session){
 
   observe({
-    # update values if auto-refresh activated
-    invalidateLater(10000, session)
-    
     if (input$checkbox == TRUE) {
+      invalidateLater(15000, session)
+      
       ws <- azureml::get_workspace(ws_name, subscription_id, rg)
       exp <- azuremlsdk::experiment(ws, exp_name)
       run <- azuremlsdk::get_run(exp, run_id)
       details <- azuremlsdk::get_run_details(run)
-
+      
       if (run_details_plot$x$data$V2[[3]] == "-" &&
           !is.null(details$startTimeUtc)) {
-          start_date_time <- as.POSIXct(details$startTimeUtc,
-                                        "%Y-%m-%dT%H:%M:%S",
-                                        tz = "UTC")
-          run_details_plot$x$data$V2[[3]] <<- format(start_date_time,
-                                                     "%B %d, %Y %I:%M %p",
-                                                     tz = Sys.timezone(),
-                                                     use_tz = TRUE)
-        }
-      }
-
-      # update duration
-      if (details$status %in% c("Failed", "Completed", "Canceled")) {
         start_date_time <- as.POSIXct(details$startTimeUtc,
                                       "%Y-%m-%dT%H:%M:%S",
                                       tz = "UTC")
-        end_date_time <- as.POSIXct(details$endTimeUtc,
+        run_details_plot$x$data$V2[[3]] <<- format(start_date_time,
+                                                   "%B %d, %Y %I:%M %p",
+                                                   tz = Sys.timezone(),
+                                                   use_tz = TRUE)
+      }
+    
+    # update duration
+    if (details$status %in% c("Failed", "Completed", "Canceled")) {
+      start_date_time <- as.POSIXct(details$startTimeUtc,
                                     "%Y-%m-%dT%H:%M:%S",
                                     tz = "UTC")
-        run_details_plot$x$data$V2[[4]] <<- 
-          paste(round(as.numeric(difftime(end_date_time,
-                                          start_date_time,
-                                          units = "mins")),
-                      digits = 2), "mins")
-      }
-      
-      # update run status
-      run_details_plot$x$data$V2[[2]] <<- details$status
-      
-      # show any error info if failed run
-      if (details$status == "Failed" &&
-          !("Errors" %in% run_details_plot$x$data$V1)) {
-        error <- details$error$error$message
-        
-        if (is.null(error)) {
-          error <- "Detailed error not set on the Run. Please check
-                    the logs for details."
-        }
-        run_details_plot$x$data <<- rbind(run_details_plot$x$data,
-                                          data.frame(V1 = "Errors",
-                                                     V2 = error))
-      }
+      end_date_time <- as.POSIXct(details$endTimeUtc,
+                                  "%Y-%m-%dT%H:%M:%S",
+                                  tz = "UTC")
+      run_details_plot$x$data$V2[[4]] <<- 
+        paste(round(as.numeric(difftime(end_date_time,
+                                        start_date_time,
+                                        units = "mins")),
+                    digits = 2), "mins")
     }
     
-    output$runDetailsPlot <- renderDataTable({
-      run_details_plot
-    })
+    # update run status
+    run_details_plot$x$data$V2[[2]] <<- details$status
+    
+    # show any error info if failed run
+    if (details$status == "Failed" &&
+        !("Errors" %in% run_details_plot$x$data$V1)) {
+      error <- details$error$error$message
+      
+      if (is.null(error)) {
+        error <- "Detailed error not set on the Run. Please check
+                    the logs for details."
+      }
+      run_details_plot$x$data <<- rbind(run_details_plot$x$data,
+                                        data.frame(V1 = "Errors",
+                                                   V2 = error))
+    }
+    }
+  })
+
+  output$runDetailsPlot <- renderDataTable({
+    invalidateLater(10000, session)
+    run_details_plot
   })
 }
 
