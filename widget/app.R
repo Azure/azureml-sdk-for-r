e20f9894fa73e6c090dc1d90bdbc6985597d6ef2
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2019-11-12 15:39:21 -0800

    collapse RUN_TERMINATED and INITIALIZED into widget_state reactive value

--- widget/app.R
+++ widget/app.R
@@ -4,44 +4,39 @@ suppressMessages(library(DT))
 
 server <- function(input, output, session) {
   
-  values <- reactiveValues(INITIALIZED = FALSE,
-                           curr_run = NULL,
-                           RUN_TERMINATED = FALSE)
+  values <- reactiveValues(widget_state = "initializing",
+                           current_run = NULL)
 
-  # retrieve run object once initialized
-  observeEvent((values$INITIALIZED), {
+  # retrieve run object once widget_state
+  observeEvent(values$widget_state == "streaming", {
     ws <- azuremlsdk::get_workspace(ws_name, subscription_id, rg)
     exp <- azuremlsdk::experiment(ws, exp_name)
-    values$curr_run <- azuremlsdk::get_run(exp, run_id)
+    values$current_run <- azuremlsdk::get_run(exp, run_id)
   },
   ignoreInit = TRUE,
   once = TRUE)
-
-  # stop app when run reaches terminal state
-  observeEvent((values$RUN_TERMINATED), {
-    shiny::stopApp()
-  },
-  ignoreInit = TRUE)
 
   # stop app if user closes session
   session$onSessionEnded(function() {
     shiny::stopApp()
   })
-
+  
   plot <- function() {
-    if (!is.null(values$curr_run)) {
-      if (isolate(values$curr_run$status) %in% c("Failed", "Completed", "Canceled")) {
+    if (!is.null(values$current_run)) {
+      if (isolate(values$current_run$status) %in% 
+          c("Failed", "Completed", "Canceled")) {
         print("Your run has reached a terminal state. The widget will close now.")
-        isolate(values$RUN_TERMINATED <- TRUE)
+        Sys.sleep(3)
+        shiny::stopApp()
       }
 
-      run_details_plot <- azuremlsdk::view_run_details(values$curr_run,
+      run_details_plot <- azuremlsdk::view_run_details(values$current_run,
                                                        auto_refresh = FALSE)
     } else {
       # initialize auto-refresh 10 seconds after job submitted
-      if (isolate(!values$INITIALIZED) &&
+      if (isolate(values$widget_state == "initializing") &&
           difftime(Sys.time(), start_time, units = "secs") > 10) {
-        isolate(values$INITIALIZED <- TRUE)
+        values$widget_state <- "streaming"
       }      
     }
 
