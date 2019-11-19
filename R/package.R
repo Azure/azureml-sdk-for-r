9193050c5434fbde495d16aa18ae67fbfbb676d9
Billy Hu <ninhu@microsoft.com>
2019-11-18 20:46:22 -0800

    override workspace python __repr__

--- R/package.R
+++ R/package.R
@@ -12,6 +12,13 @@
       # This function will be called on successful load
       ver <- toString(utils::packageVersion("azuremlsdk"))
       azureml$"_base_sdk_common"$user_agent$append("azureml-r-sdk", ver)
+      
+      azureml$core$Workspace$"__repr__" <- function(self) {
+        sprintf("create_workspace(name=\"%s\", subscription_id=\"%s\", resource_group=\"%s\")", 
+          self$"_workspace_name",
+          self$"_subscription_id",
+          self$"_resource_group")
+        }
     },
 
     on_error = function(e) {
