1c17805b0da85f56681383234ddc2950c6b23218
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2019-11-22 09:13:27 -0800

    fix code quality

--- inst/widget/app.R
+++ inst/widget/app.R
@@ -4,9 +4,9 @@
 suppressMessages(library(shiny))
 suppressMessages(library(DT))
 
-
+#nolint start
 server <- function(input, output, session) {
-  
+
   values <- reactiveValues(widget_state = "initializing",
                            current_run = NULL)
 
@@ -23,36 +23,37 @@ server <- function(input, output, session) {
   session$onSessionEnded(function() {
     shiny::stopApp()
   })
-  
+
   plot <- function() {
 
     if(isolate(values$widget_state) %in% c("initializing", "streaming")) {
       invalidateLater(10000, session)
     }
-  
+
     if (!is.null(values$current_run)) {
       run_details_plot <- azuremlsdk::view_run_details(values$current_run,
                                                        auto_refresh = FALSE)
       if (isolate(values$widget_state == "streaming") &&
           values$current_run$status %in% c("Canceled", "Completed", "Failed")) {
         values$widget_state <- "finalizing"
-        print("Your run has reached a terminal state. You may close the widget now.")
+        print("The run has reached a terminal state. You may close the widget.")
       }
     } else {
       # initialize auto-refresh 10 seconds after job submitted
       if (isolate(values$widget_state == "initializing") &&
           difftime(Sys.time(), start_time, units = "secs") > 10) {
         values$widget_state <- "streaming"
-      }      
+      }
     }
 
     return(run_details_plot)
   }
-  
+
   output$runDetailsPlot <- DT::renderDataTable({
     plot()
   })
 }
+#nolint end
 
 ui <- fluidPage(
   shinycssloaders::withSpinner(dataTableOutput("runDetailsPlot"),
