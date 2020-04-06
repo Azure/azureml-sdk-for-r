ff904a7813e89bf0324073b5d3af85e351f8e8f8
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2020-04-06 13:40:24 -0700

    remove line + add seealso

--- R/datasets.R
+++ R/datasets.R
@@ -378,7 +378,6 @@ create_tabular_dataset_from_json_lines_files <- function(
 #' datastore <- get_datastore(ws, 'sql-db')
 #' query <- data_path(datastore, 'SELECT * FROM my_table')
 #' tab_ds <- create_tabular_dataset_from_sql_query(query, query_timeout = 10)
-#' df <- load_dataset_into_data_frame(tab_ds)
 #'
 #' # use `set_column_types` param to set column data types
 #' data_types <- list(ID = data_type_string(),
@@ -390,8 +389,8 @@ create_tabular_dataset_from_json_lines_files <- function(
 #' set_tab_ds <- create_tabular_dataset_from_sql_query(query, set_column_types = data_types)
 
 #' ```
-#' @seealso
-#' \code{\link{data_path}}
+#' @seealso [data_path()] [data_type_datetime()] [data_type_bool()]
+#' [data_type_double()] [data_type_string()] [data_type_long()] 
 #' @md
 create_tabular_dataset_from_sql_query <- function(query, validate = TRUE,
                                                   set_column_types = NULL,
