cf71584d8d85fee04cc6612a344596707f6c28e9
Diondra <16376603+diondrapeck@users.noreply.github.com>
2020-08-25 14:09:40 -0700

    Fix compute_target config failure in batch_inferencing.R (#383)
    * update version
    
    * update
    
    * set up compute target every time
    
    * shorten name
    
    * revert

--- samples/foreach/batch_inferencing/batch_inferencing.R
+++ samples/foreach/batch_inferencing/batch_inferencing.R
@@ -11,17 +11,14 @@ devtools::load_all()
 ws <- load_workspace_from_config()
 
 # create AmlCompute cluster
-cluster_name <- "cpu-cluster"
-compute_target <- get_compute(ws, cluster_name = cluster_name)
-if (is.null(compute_target)) {
-  vm_size <- "STANDARD_D2_V2"
-  compute_target <- create_aml_compute(workspace = ws,
-                                       cluster_name = cluster_name,
-                                       vm_size = vm_size,
-                                       max_nodes = 3)
+cluster_name <- "sample-cluster"
+vm_size <- "STANDARD_D2_V2"
+compute_target <- create_aml_compute(workspace = ws,
+                                     cluster_name = cluster_name,
+                                     vm_size = vm_size,
+                                     max_nodes = 3L)
   
-  wait_for_provisioning_completion(compute_target, show_output = TRUE)
-}
+wait_for_provisioning_completion(compute_target, show_output = TRUE)
 
 # call this method to register foreach backend with Workspace and AmlCompute cluster on which
 # parallel job would run.
